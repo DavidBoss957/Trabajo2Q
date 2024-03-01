8940a2ad76fd4bfbab3403af3a1ac29eaef70f73
d4rkpsych0p4th <33335436+d4rkpsych0p4th@users.noreply.github.com>
2024-03-01 22:09:28 +0100

    validator user fix
    -add noempty()

--- backend/validators/users.js
+++ backend/validators/users.js
@@ -5,9 +5,9 @@ const emailRegex = /@(live\.u-tad\.com|u-tad\.com|ext\.u-tad\.com)$/;
 // Validador para la creación de usuarios
 const validatorCreateUser = [
     body('name').isString().notEmpty(),
-    body('apellido1').isString(),
-    body('apellido2').isString(),
-    body('alias').isString(),
+    body('apellido1').isString().notEmpty(),
+    body('apellido2').isString().notEmpty(),
+    body('alias').isString().notEmpty(),
     body('cargo').isString().isIn(["alumno", "alumni", "profesor", "coordinador", "departamento"]).notEmpty(),
     body('email').isEmail().matches(emailRegex).withMessage('El correo electrónico debe ser de @live.u-tad.com o @u-tad.com o @ext.u-tad.com').notEmpty(),
     body('password').isString().notEmpty(),
