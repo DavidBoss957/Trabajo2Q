b806dea37aa2d063ab4ec8fe798501258c361588
albalk <allopcarru@gmail.com>
2024-03-02 01:00:31 +0100

    login con bootstrap
    diseño de los wireframes implementado

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,14 +1,28 @@
 "use client";
 import Head from 'next/head';
 import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Login() {
     
     let [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [emailType, setEmailType] = useState("")
+    const [emailType, setEmailType] = useState("@live.u-tad.com")
 
     const handleSubmit = (e) => {
+        //e.preventDefault();
+
+        const login = {
+            email: email+emailType,
+            password: password
+        }
+
+        //TODO añadir llamada fetch
+        alert("Iniciando sesion...")
+           
+    }
+
+    const handleComprobacion = (e) => {
         e.preventDefault();
         
         //comprobaciones
@@ -16,48 +30,55 @@ export default function Login() {
             //si el input del email contiene @, se elimina todo el contenido despues del @
             email = email.substring(0, email.indexOf("@"))
         }
-        // if(emailType==""){
-        //     //TODO sacarlo del handlesubmit para que no se envien con errores
-        //     alert("Selecciona un tipo de email")
-        // }
-
-        //añadir comprobaciones de contraseñas
-
-
-        const login = {
-            email: email+emailType,
-            password: password
+        if(emailType==""){
+            //TODO sacarlo del handlesubmit para que no se envien con errores
+            alert("Selecciona un tipo de email")
         }
 
-        //alert("Email: " + email + emailType + "\nContraseña: " + password);
-        
-        //TODO añadir llamada fetch
+        alert("Email: " + email + emailType + "\nContraseña: " + password);
+
+        handleSubmit()
            
     }
 
     return (
-        <div className="container">
-            <h2>Iniciar sesión</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Correo</label>
-                    <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} required />
-                    <select id="emailType" onChange={(e) => setEmailType(e.target.value)}>
-                        <option value="">@</option>
-                        <option value="@live.u-tad.com">@live.u-tad.com</option>
-                        <option value="@u-tad.com">@u-tad.com</option>
-                    </select>
-                </div>
-                
-                
-                <div>
-                    <label htmlFor="password">Contraseña</label>
-                    <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} required />
-                </div>
 
-                <button type='submit'>Log In</button>
-            </form>
+        <div id="loginContent">
+                        
+            <img src="img/default.png" className="img-fluid mx-auto d-block mt-5" alt="default"/>
+            
+            <h1 id="titulo" className="text-center my-4">Introducción</h1>
 
+            <div id="containerFormulario" className="d-flex justify-content-center mx-5 my-5 ">
+                
+                <form onSubmit={handleComprobacion} className="px-5 py-5 rounded-4 bg-secondary">
+                    <h2>Iniciar sesión</h2>
+                    {/* correo */}
+                    <label htmlFor="email">Dirección de correo electrónico</label>
+                    <div className="input-group mb-3 ">
+                        <input type="text" id="email" className="form-control rounded-start-4"  onChange={(e) => setEmail(e.target.value)} placeholder="mario.hurtado" required/>
+                        <select id="emailType" className="dropdown-toggle btn bg-light rounded-end-4" onChange={(e) => setEmailType(e.target.value)}>
+                            <option value="@live.u-tad.com">@live.u-tad.com</option>
+                            <option value="@u-tad.com">@u-tad.com</option>
+                            <option value="@ext.live.u-tad.com">@ext.live.u-tad.com</option>
+                        </select>
+                    </div>
+
+                    {/*contraseña */}
+                    <label htmlFor='password'>Contraseña</label>
+                    <div className="mb-3">
+                        <input type="password" id='password' className="form-control rounded-4" onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" required/>
+                    </div>
+
+                    {/*boton submit*/}
+                    <div className='text-center'>
+                        <button type="submit" className="btn btn-light rounded-4 mt-4 text-center">Acceder</button>
+                    </div>
+                    
+
+                </form>            
+            </div>
         </div>
+        
     );
 }
