9991f588bd8e382f532e48f282c3e915ec147cd5
Sp00kySc4rySk3l3t0n <franciscojaviersanchezcano1@gmail.com>
2024-04-04 23:59:50 +0200

    Modificaciones Demo
    Añadido:
    Filtro añadido a la pagina de inicio
    Ahora se pueden visualizar proyectos
    
    Modificaciones:
    Colores del filtro modificado para que sea mas armonioso

--- demo/app/MainPage/page.js
+++ demo/app/MainPage/page.js
@@ -1,10 +1,31 @@
 'use client'
 
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from 'next/head';
 import { useRouter } from 'next/navigation'
 
 export default function MainPage() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showFilters, setShowFilters] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState("Recomendado");
+  const [selectedFilter, setSelectedFilter] = useState("Todos los proyectos");
+
+  const handleSearchChange = (e) => setSearchQuery(e.target.value);
+  const toggleFilters = () => setShowFilters(!showFilters);
+  const handleOrderChange = (e, newOrder) => {
+    e.preventDefault();
+    setSelectedOrder(newOrder);
+  };
+  const handleFilterChange = (newFilter) => {
+    setSelectedFilter(newFilter);
+  };
+
+  // Renderiza el contenido basado en el filtro seleccionado
+  const renderContent = () => {
+  };
+
+
   const router = useRouter();
 
   const handleProfileClick = () => {
@@ -31,7 +52,7 @@ export default function MainPage() {
             <img src="U-TAD-Logo-CARD.webp" alt="Logo" className="img-fluid" style={{ maxHeight: '60px' }} />
             <div>
               <button className="btn btn-outline-primary mx-2" onClick={handleProfileClick}>Mi perfil</button>
-              <button className="btn btn-primary" onClick={handleUploadClick}>Subir proyecto</button>
+              <button className="btn btn-primary" onClick={handleUploadClick}>Crear Proyecto</button>
             </div>
           </header>
         </div>
@@ -51,13 +72,66 @@ export default function MainPage() {
       </div>
 
       {/* Botones de navegación, fuera del fondo */}
-      <div className="container my-4">
-        <div className="d-flex justify-content-center">
-          <button className="btn btn-outline-primary mx-3 my-2">Grados</button>
-          <button className="btn btn-outline-primary mx-3 my-2">Ciclos</button>
-          <button className="btn btn-outline-primary mx-3 my-2">Postgrados</button>
+        <div className="d-flex justify-content-between m-4">
+          <div className="d-flex">
+            <button onClick={() => handleFilterChange("Todos los proyectos")} className={`btn ${selectedFilter === "Todos los proyectos" ? "btn-primary" : "btn-outline-primary"} me-2`}>Todos los proyectos</button>
+            <button onClick={() => handleFilterChange("Proyectos premiados")} className={`btn ${selectedFilter === "Proyectos premiados" ? "btn-primary" : "btn-outline-primary"} me-2`}>Proyectos premiados</button>
+            <button onClick={() => handleFilterChange("Autor/es")} className={`btn ${selectedFilter === "Autor/es" ? "btn-primary" : "btn-outline-primary"} me-2`}>Autor/es</button>
+          </div>
+          <div className="d-flex">
+            {/* This div now wraps both the Mostrar Filtros button and the Ordenar por dropdown */}
+            <button onClick={toggleFilters} className="btn btn-outline-primary me-2">Mostrar Filtros</button>
+            <div className="dropdown"></div>
+          <div className="dropdown">
+            <button className="btn btn-outline-primary dropdown-toggle" type="button" id="dropdownOrderButton" data-bs-toggle="dropdown" aria-expanded="false">
+              Ordenar por: {selectedOrder}
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownOrderButton">
+              <li><a className="dropdown-item" href="#" onClick={(e) => handleOrderChange(e, 'Recomendado')}>Recomendado</a></li>
+              <li><a className="dropdown-item" href="#" onClick={(e) => handleOrderChange(e, 'Más reciente primero')}>Más reciente primero</a></li>
+              <li><a className="dropdown-item" href="#" onClick={(e) => handleOrderChange(e, 'Más antiguo primero')}>Más antiguo primero</a></li>
+            </ul>
+          </div>
         </div>
-      </div>
+        </div>
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          {renderContent()}
+        </div>
+
+
+        {showFilters && (
+          <div className="modal-backdrop fade show">
+            <div className="modal fade show d-block" tabIndex="-1" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+              <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Filtrar por:</h5>
+                    <button type="button" className="btn-close" onClick={toggleFilters}></button>
+                  </div>
+                  <div className="modal-body">
+                    <form>
+                      <div className="mb-3">
+                        <label htmlFor="titulo" className="form-label">Título</label>
+                        <input type="text" className="form-control" id="titulo" />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="asignatura" className="form-label">Asignatura</label>
+                        <input type="text" className="form-control" id="asignatura" />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="anoCreacion" className="form-label">Año de creación</label>
+                        <input type="number" className="form-control" id="anoCreacion" />
+                      </div>
+                      <div className="text-center">
+                        <button type="button" className="btn btn-primary" onClick={() => setShowFilters(false)}>Actualizar resultados</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
 
       {/* Sección de proyectos, también fuera del fondo */}
       <div className="container mb-4">
--- demo/app/verProyecto/page.js
+++ demo/app/verProyecto/page.js
@@ -1,8 +1,8 @@
 "use client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '../styles/subida_proyectos.css'
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation'
 
 export default function ViewProjects() {
     const [projects, setProjects] = useState(null);
@@ -22,58 +22,76 @@ export default function ViewProjects() {
     if (projects === null) {
       return <div className="container mt-3">Cargando proyectos...</div>;
     }*/
+    const router = useRouter();
+    const handleBack = () => {
+      router.push('/MainPage/proyecto');
+  };
   
     return (
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col">
-            <h1>Título del proyecto</h1>
-            <div className="mb-3">
-              <label className="form-label">Autores</label>
-              {/* Componente o elementos de lista de autores aquí */}
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Docentes implicados</label>
-              {/* Componente o elementos de lista de docentes aquí */}
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Asignatura/s: Proyectos IV</label>
-              {/* Otros detalles de la asignatura si son necesarios */}
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Año de creación: 2024</label>
-              {/* Otros detalles del año si son necesarios */}
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Resultado final</label>
-              {/* Espacio para el resultado final aquí, por ejemplo, una imagen o texto */}
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Resumen</label>
-              <p>
-                {/* Texto del resumen aquí */}
-              </p>
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Descripción y factor diferenciador de la propuesta</label>
-              <p>
-                {/* Texto de la descripción aquí */}
-              </p>
-            </div>
-            {/* Añadir más secciones según sea necesario */}
-            <div className="mb-3 text-center">
-              <button className="btn btn-primary me-2">Modificar proyecto</button>
-              <button className="btn btn-danger">Eliminar proyecto</button>
-            </div>
-            {/* Hash tags aquí */}
-            <div className="mb-3">
-              <p>#hashtag #hashtag #hashtag #hashtag #hashtag</p>
-            </div>
-            <div className="text-center">
-              <button className="btn btn-secondary">Volver atrás</button>
-            </div>
-          </div>
+<div className="container my-5">
+  <div className="row">
+    <div className="col-lg-10 mx-auto">
+      {/* Título del Proyecto */}
+      <h1 className="mb-3">"OptiCode: Plataforma de optimización para algoritmos en la nube"</h1>
+      
+      {/* Autores */}
+      <div className="mb-3">
+        <h5>Autores:</h5>
+        <p className="d-inline ms-3">Jaime Alberto Serrano y Sonia Ramirez López</p>
+      </div>
+
+      {/* Docentes Implicados */}
+      <div className="mb-3">
+        <h5>Docentes implicados:</h5>
+        <p className="d-inline ms-3">Dr. Carlos Fernández Linares</p>
+      </div>
+
+      {/* Asignaturas */}
+      <div className="mb-3">
+        <h5>Asignatura/s:</h5>
+        <p className="d-inline ms-3">Desarrollo Avanzado de Software</p>
+      </div>
+
+      {/* Año de creación */}
+      <div className="mb-3">
+        <h5>Año de creación:</h5>
+        <p className="d-inline ms-3">2024</p>
+      </div>
+
+      {/* Resultado final */}
+      <div className="mb-3">
+        <h5>Resultado final</h5>
+        <img src="/tipos-de-almacenamiento-en-la-nube.jpg" className="card-img-top w-50 h-auto" alt="Proyecto"/>
+        <div className="mt-2">
+          <button className="btn btn-success text-white">Descargar Proyecto</button>
         </div>
       </div>
+
+      {/* Resumen */}
+      <div className="mb-3">
+        <h5>Resumen:</h5>
+        <p className="mt-2">
+          "OptiCode es una plataforma basada en la nube que permite a los desarrolladores optimizar y probar algoritmos de manera eficiente. Utiliza inteligencia artificial para identificar cuellos de botella en tiempo de ejecución y consumo de recursos, proponiendo mejoras en el código. Su valor diferencial radica en la personalización de sugerencias según el tipo de software desarrollado, ya sea para aplicaciones de alto rendimiento, juegos o sistemas embebidos."
+        </p>
+      </div>
+
+      {/* Etiquetas o Hashtags */}
+      <div className="mb-3">
+        <p className="text-info">#optimización de algoritmos, #ingeniería de software, #plataforma en la nube, #inteligencia artificial, #rendimiento de código</p>
+      </div>
+
+      {/* Botones de acción */}
+      <div className="mb-3 d-flex justify-content-end">
+        <button className="btn btn-primary me-2">Modificar proyecto</button>
+        <button className="btn btn-danger">Eliminar proyecto</button>
+      </div>
+
+      {/* Botón de regreso */}
+      <div className="text-center">
+        <button className="btn btn-secondary" onClick={handleBack}>Volver atrás</button>
+      </div>
+    </div>
+  </div>
+</div>
     );
   }
