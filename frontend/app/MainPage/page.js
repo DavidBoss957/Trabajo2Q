3157fd66c997857373c51e2b723dedb17d3ed67b
albalk <allopcarru@gmail.com>
2024-04-11 22:56:15 +0200

    Conexión con back funcional
    En ambos archivos:
    - hacer peticion get de los proyectos
    - quitar el codigo del navbar e implementarlo como componente
    - añadir mapeo de los proyectos a las tarjetas ya creadas

--- frontend/app/MainPage/page.js
+++ frontend/app/MainPage/page.js
@@ -1,14 +1,41 @@
 'use client'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/navigation'
 import '../styles/mainPage.css'
+import NavbarPrincipal from '@/components/NavbarPrincipal';
+
+
+const getProyectInfo = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/trabajos")
+
+    if(res.ok){
+      const proyectInfo = await res.json()
+      return proyectInfo
+    }else{
+      console.error("Error al recibir la información")
+    }
+
+  } catch (e) {
+    console.error("Error al hacer la petición: ", e)
+  }
+
+}
+
 
 export default function MainPage() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [showFilters, setShowFilters] = useState(false);;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showFilters, setShowFilters] = useState(false);
+  const [proyectInfo, setProyectInfo] = useState("");
+
+  useEffect(() => {
+    getProyectInfo().then(proyectInfo => {
+      setProyectInfo(proyectInfo);
+    });
+  }, []);
 
   const handleSearchChange = (e) => setSearchQuery(e.target.value);
   const toggleFilters = () => setShowFilters(!showFilters);
@@ -20,97 +47,43 @@ export default function MainPage() {
     setSelectedFilter(newFilter);
   };
 
-
   const router = useRouter();
-
-  const handleProfileClick = () => {
-    // Suponiendo que tienes una ruta "/mi-perfil" en tu aplicación de Next.js
-    router.push('/perfil');
-  };
-
-  const handleUploadClick = () => {
-    // Suponiendo que tienes una ruta "/subir-proyecto" en tu aplicación de Next.js
-    router.push('/subida_proyectos');
-  };
 
   return (
     <>
-      <Head>
-        <title>Descripción de la página</title>
-        <link href="https://fonts.googleapis.com/css?family=Poppins:300,900&display=swap" rel="stylesheet"/>
-      </Head>
-      <div className="bg-light border-bottom">
-        {/* Contenedor fluido para la parte superior con el fondo */}
-        <div className="container-fluid">
-          {/* Encabezado */}
-          {/*No se porque coño no se queda pegado al scrolear*/}
-          <nav className="navbar navbar-default navbar-sticky-top d-flex justify-content-between align-items-center py-2">
-            <img src="/U-TAD-Logo-CARD.webp" alt="Logo" className="img-fluid" style={{ maxHeight: '60px' }} />
-            <div className="d-flex align-items-center flex-grow-1 justify-content-center">
-              <form className="form-inline">
-                  <input className="form-control mr-2" type="search" placeholder="Buscar" aria-label="Buscar" style={{ minWidth: '500px' }} />
-              
-              </form>
-            </div>
-            <div>
-                <button className="btn btn-outline-primary mx-2" onClick={handleProfileClick}>Mi perfil</button>
-                <button className="btn btn-primary" onClick={handleUploadClick}>Crear Proyecto</button>
-            </div>
-          </nav>
-        </div>
-         {/* Contenedor para el título y la barra de búsqueda */}
-         <div className="container my-3">
-          {/* Título y descripción de la página */}
-          <div className="text-center py-4">
-            <h1 id="titulo" className="display-1 fw-bold text-primary">AcademiaLink</h1>
-          </div>
-          
-          {/* Barra de búsqueda */}
-          <div className="input-group mb-4">
-            <input type="text" className="form-control" placeholder="Barra de búsqueda" />
-          </div>
-        </div>
-
-
-      </div>
+      
+      <NavbarPrincipal />
 
       {/* Botones Orientativos */}
-      <div class="home-about">
-      
-          <ul class="nav__list">
-            
-              <a class=" grados ">Grados</a>
-            
-            
-              <a class="ciclos" >Ciclos</a>
-            
-            
-              <a class="postgrados">Postgrados</a>
-            
+      <div className="home-about">
+          <ul className="nav__list">
+              <a className=" grados ">Grados</a>
+              <a className="ciclos" >Ciclos</a>
+              <a className="postgrados">Postgrados</a>
           </ul>
       </div>
 
       {/* Sección de proyectos, también fuera del fondo */}
       <div className="container mt-4">
         <div className="row">
-          {Array.from({ length: 12 }).map((_, idx) => (
-            <div key={idx} className="col-md-4 mb-3">
-              {/* Se utiliza un anchor tag para hacer toda la tarjeta clicable */}
-              <a href="/verProyecto" className="card-link-custom">
-                <div className="card h-100 tarjeta"> {/* Asegúrate de que la tarjeta ocupe toda la altura para que el enlace también lo haga */}
-                  <div className="card-header card-header-titulacion text-center">
-                    Grado en Ingeniería de Software
+            {Array.isArray(proyectInfo) && proyectInfo.map((proyecto) => (
+              <div className="col-md-4 mb-3" key={proyecto._id}>
+                {/* Se utiliza un anchor tag para hacer toda la tarjeta clicable */}
+                <a href={proyecto.enlace} className="card-link-custom">
+                  <div className="card h-100 tarjeta"> {/* Asegúrate de que la tarjeta ocupe toda la altura para que el enlace también lo haga */}
+                    <div className="card-header card-header-titulacion text-center">
+                      {proyecto.titulacion}
+                    </div>
+                    <img src={proyecto.resultadofinal.url} className="card-img-top" alt="Proyecto" /> 
+                    <div className="card-body">
+                      <h6 className="card-title">{proyecto.titulo}</h6>
+                      <p className="card-text">{proyecto.autores.map((autor) => autor)}</p>
+                      <p className="card-text text-right">{proyecto.anocreacion}</p>
+                    </div>
                   </div>
-                  <img src="/tipos-de-almacenamiento-en-la-nube.jpg" className="card-img-top" alt="Proyecto" />
-                  <div className="card-body">
-                    <h6 className="card-title">OptiCode: Plataforma de optimización para algoritmos en la nube</h6>
-                    <p className="card-text">Jaime Alberto Serrano y Sonia Ramirez López</p>
-                    <p className="card-text text-right">2024</p>
-                  </div>
-                </div>
-              </a>
-            </div>
-          ))}
+                </a>
+              </div>
+            ))}
         </div>
       </div>
     </>
--- frontend/app/MainPageSearched/page.js
+++ frontend/app/MainPageSearched/page.js
@@ -1,16 +1,43 @@
 'use client'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/navigation'
 import '../styles/MainPage.css'
+import NavbarSecundario from '@/components/NavbarSecundario';
+
+const getProyectInfo = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/trabajos")
+
+    if(res.ok){
+      const proyectInfo = await res.json()
+      return proyectInfo
+    }else{
+      console.error("Error al recibir la información")
+    }
+
+  } catch (e) {
+    console.error("Error al hacer la petición: ", e)
+  }
+
+}
 
 export default function MainPage() {
   const [searchQuery, setSearchQuery] = useState('');
   const [showFilters, setShowFilters] = useState(false);
   const [selectedOrder, setSelectedOrder] = useState("Recomendado");
   const [selectedFilter, setSelectedFilter] = useState("Todos los proyectos");
+  const [proyectInfo, setProyectInfo] = useState("");
+
+  useEffect(() => {
+    getProyectInfo().then(proyectInfo => {
+      setProyectInfo(proyectInfo);
+    });
+  }, []);
+
+  console.log(proyectInfo)
 
   const handleSearchChange = (e) => setSearchQuery(e.target.value);
   const toggleFilters = () => setShowFilters(!showFilters);
@@ -40,30 +67,8 @@ export default function MainPage() {
 
   return (
     <>
-      <Head>
-        <title>Descripción de la página</title>
-        <link href="https://fonts.googleapis.com/css?family=Poppins:300,900&display=swap" rel="stylesheet"/>
-      </Head>
-      <div className="bg-light border-bottom">
-        {/* Contenedor fluido para la parte superior con el fondo */}
-        <div className="container-fluid">
-          {/* Encabezado */}
-          {/*No se porque coño no se queda pegado al scrolear*/}
-          <nav className="navbar navbar-default navbar-sticky-top d-flex justify-content-between align-items-center py-2">
-            <img src="/U-TAD-Logo-CARD.webp" alt="Logo" className="img-fluid" style={{ maxHeight: '60px' }} />
-            <div className="d-flex align-items-center flex-grow-1 justify-content-center">
-              <form className="form-inline">
-                  <input className="form-control mr-2" type="search" placeholder="Buscar" aria-label="Buscar" style={{ minWidth: '500px' }} />
-              
-              </form>
-            </div>
-            <div>
-                <button className="btn btn-outline-primary mx-2" onClick={handleProfileClick}>Mi perfil</button>
-                <button className="btn btn-primary" onClick={handleUploadClick}>Crear Proyecto</button>
-            </div>
-          </nav>
-        </div>
-      </div>
+      
+      <NavbarSecundario />
 
       {/* Botones de navegación, fuera del fondo */}
       <div className="d-flex justify-content-between m-4">
@@ -130,19 +135,19 @@ export default function MainPage() {
       {/* Sección de proyectos, también fuera del fondo */}
       <div className="container mt-4">
         <div className="row">
-          {Array.from({ length: 12 }).map((_, idx) => (
-            <div key={idx} className="col-md-4 mb-3">
+          {Array.isArray(proyectInfo) && proyectInfo.map((proyecto) => (
+            <div className="col-md-4 mb-3" key={proyecto._id}>
               {/* Se utiliza un anchor tag para hacer toda la tarjeta clicable */}
-              <a href="/verProyecto" className="card-link-custom">
+              <a href={proyecto.enlace} className="card-link-custom">
                 <div className="card h-100 tarjeta"> {/* Asegúrate de que la tarjeta ocupe toda la altura para que el enlace también lo haga */}
                   <div className="card-header card-header-titulacion text-center">
-                    Grado en Ingeniería de Software
+                    {proyecto.titulacion}
                   </div>
-                  <img src="/tipos-de-almacenamiento-en-la-nube.jpg" className="card-img-top" alt="Proyecto" />
+                  <img src={proyecto.resultadofinal.url} className="card-img-top" alt="Proyecto" /> 
                   <div className="card-body">
-                    <h6 className="card-title">OptiCode: Plataforma de optimización para algoritmos en la nube</h6>
-                    <p className="card-text">Jaime Alberto Serrano y Sonia Ramirez López</p>
-                    <p className="card-text text-right">2024</p>
+                    <h6 className="card-title">{proyecto.titulo}</h6>
+                    <p className="card-text">{proyecto.autores.map((autor) => autor)}</p>
+                    <p className="card-text text-right">{proyecto.anocreacion}</p>
                   </div>
                 </div>
               </a>
