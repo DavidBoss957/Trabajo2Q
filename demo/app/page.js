c5d62075523dac78346a46f7cad0a274e40cafa0
Sp00kySc4rySk3l3t0n <franciscojaviersanchezcano1@gmail.com>
2024-05-03 22:03:11 +0200

    Estilo demo login

--- demo/app/page.js
+++ demo/app/page.js
@@ -3,7 +3,8 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Head from 'next/head';
+import './globals.css'
+import './Usuario/styles/login_signup.css'
 
 export default function Login() {
     let [email, setEmail] = useState("") //input email
@@ -18,21 +19,21 @@ export default function Login() {
     }
 
     return (
-        <div id="loginContent" className="container mt-3">
+        <div id="loginContent" className="">
             <div src="x" className="mx-auto d-block mt-6"></div>
-                <h1 id="titulo" className="text-center my-4 display-1 fw-bold text-primary">AcademiaLink</h1>
+                <h1 id="titulo" className="text-center my-4 display-1 fw-bold">¡Bienvenid@ a U-tad Projects!</h1>
 
-                <div id="containerFormulario" className="d-flex justify-content-center mx-5 my-5 shadow-lg">
-                    <form className="bg-white p-5 rounded-4" onSubmit={handleSubmit} >
+                <div id="containerFormulario" className="d-flex justify-content-center mx-5 my-5 shadow-lg center">
+                    <form className="" onSubmit={handleSubmit} >
                         
-                        <h2 className="text-center mb-4">Iniciar sesión</h2>
+                        <h2 id="iniciar" className="text-center mb-4">Iniciar sesión</h2>
                         
                         {/* correo */}
                         <div className="mb-3">
-                            <label htmlFor="email" className="form-label">Dirección de correo electrónico</label>
-                            <div className="input-group">
-                                <input type="text" id="email" className="form-control rounded-start"  onChange={(e) => setEmail(e.target.value)} placeholder="mario.hurtado" required/>
-                                <select id="emailType" className="form-select rounded-end" onChange={(e) => setEmailType(e.target.value)}>
+                            <label htmlFor="email" className="direccion body_type">Dirección de correo electrónico*</label>
+                            <div className="input-group input_direccion">
+                                <input type="text" id="email" className="form-control rounded-start body_type"  onChange={(e) => setEmail(e.target.value)} placeholder="mario.hurtado" required/>
+                                <select id="emailType" className="form-select rounded-end body_type" onChange={(e) => setEmailType(e.target.value)}>
                                     <option value="@live.u-tad.com">@live.u-tad.com</option>
                                     <option value="@u-tad.com">@u-tad.com</option>
                                     <option value="@ext.live.u-tad.com">@ext.live.u-tad.com</option>
@@ -42,13 +43,13 @@ export default function Login() {
 
                         {/*contraseña */}
                         <div className="mb-4">
-                            <label htmlFor='password' className="form-label">Contraseña</label>
-                            <input type="password" id='password' className="form-control rounded" onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" required/>
+                            <label htmlFor='password' className="contrasena body_type">Contraseña*</label>
+                            <input type="password" id='password' className="input_contrasena form-control rounded body_type" onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" required/>
                         </div>
 
                         {/*boton submit*/}
-                        <div className="text-center">
-                            <button type="submit" className="btn btn-primary mt-2 text-center rounded px-5">Acceder</button>
+                        <div className="layout_boton_acceder">
+                            <button type="submit" id="boton_acceder" className="body_type text_botton_acceder">Acceder</button>
                         </div>
 
                     </form>
